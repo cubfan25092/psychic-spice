04e2c0402cb9c6c2a3cf23e6dfefc28cc51d9e66
Paulie Davis <cubfan2509@gmail.com>
2013-12-18 21:38:41 -0600

    added accuracy computation, reduced runtime

--- hw7.py
+++ hw7.py
@@ -5,26 +5,25 @@ import sys;
 import os;
 import matplotlib.pyplot as pl;
 
-def plot(ws, bs, lams,vs):
-	print ws;
+def plot(lambdas, acc, wtw):
 	pl.figure(0);
 	x = np.linspace(1,1000,1000);
-	results = np.zeros((1000));
-	for j in xrange(1):
-		for i in xrange(1000):
-			results[i] = np.linalg.norm(ws[i+(j*1000)]);
-		pl.plot(x,results, label=str(lams[j]));
+	for j in xrange(10):
+		pl.plot(x,wtw[1000*j:1000*(j+1)], label=str(lambdas[j]));
 	pl.legend();		
 	pl.show();
-	figure(1);
-	error = np.zeros((1000));
-	
+	pl.figure(1);
+	for j in xrange(10):
+		pl.plot(x,acc[1000*j:1000*(j+1)], label=str(lambdas[j]));
+	pl.legend();
+	pl.show();
 	return 0;
 
-def run_epoch_pt1(epoch_num, lam, xy):
+def run_epoch_pt1(epoch_num, lam, xy, vs):
 	steplength = 1.0/((.01 * epoch_num) + 50);
 	w = np.zeros((1,6));
 	b = 0;
+	num_correct = 0.0;
 	for i in xy:
 		yk = i[6];
 		xk = i[0:6];
@@ -33,7 +32,13 @@ def run_epoch_pt1(epoch_num, lam, xy):
 		else:
 			w = w - (steplength * ((lam * w) - (yk * xk)));
 			b = b - (steplength *((-1) * yk));
-	return w, b;
+	for i in vs:
+		yk = i[6];
+		xk = i[0:6];
+		d = np.dot(w, xk);
+		if yk * (d + b) >= 1:
+			num_correct += 1.0;
+	return w, b, num_correct/1000.0;
 
 
 def pt1(xy):
@@ -47,19 +52,18 @@ def pt1(xy):
 	w = np.zeros((10*1000,6));
 	b = np.zeros((10*1000));
 	lam = 1e-7;
-	curr_best = 0;
-	curr_best_lambda = 0;
 	total_epochs = 0;
-	results = np.zeros((10));
-	for j in xrange(1):
-		results[j] = lam;
+	lambdas = np.zeros((10));
+	wtw = np.zeros((10*1000));
+	acc = np.zeros((10*1000));
+	for j in xrange(10):
+		lambdas[j] = lam;
 		for k in xrange(1000):
-			np.random.shuffle(rest);
-			w[total_epochs], b[total_epochs] = run_epoch_pt1(k, lam, rest[:1000, :]);
+			w[total_epochs], b[total_epochs], acc[total_epochs] = run_epoch_pt1(k+1, lam, rest[np.random.choice(rest.shape[0], 1000),:], validation_set);
+			wtw[total_epochs] = np.linalg.norm(w[total_epochs]);
 			total_epochs += 1;
 		lam *= 10;
-		print "new lambda generated"
-	plot(w,b,results,validation_set);
+	plot(lambdas, acc, wtw);
 	
 
 
@@ -71,9 +75,10 @@ def parse(fname):
 	numeric_data = raw_data[:,includes_pt1];
 	for x in numeric_data:
 		if x[6] == "<=50K":
-			x[6] = "-1"
+			x[6] = "-1";
 		else:
-			x[6] = "1"
+			x[6] = "1";
+	np.random.shuffle(numeric_data);
 	return pt1(numeric_data.astype(np.float));
 
 
